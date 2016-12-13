be0c6f6a9b06ba190946d381bf3a70dbc9b9c7dc
Yuta Koshizawa <koher@koherent.org>
2016-12-13 19:28:50 +0900

    Implement typed `convoluted` for `RGBA`

--- src/org/koherent/image/RGBA.kt
+++ src/org/koherent/image/RGBA.kt
@@ -30,3 +30,51 @@ val RGBA<Float>.gray: Float
 
 val RGBA<Double>.gray: Double
     get() = (red + green + blue) / 3
+
+internal infix fun RGBA<Int>.plus_ii(other: RGBA<Int>): RGBA<Int> {
+    return RGBA(red + other.red, green + other.green, blue + other.blue, alpha + other.alpha)
+}
+
+internal infix fun RGBA<Float>.plus_ff(other: RGBA<Float>): RGBA<Float> {
+    return RGBA(red + other.red, green + other.green, blue + other.blue, alpha + other.alpha)
+}
+
+internal infix fun RGBA<Double>.plus_dd(other: RGBA<Double>): RGBA<Double> {
+    return RGBA(red + other.red, green + other.green, blue + other.blue, alpha + other.alpha)
+}
+
+internal infix fun RGBA<Byte>.times_bi(k: Int): RGBA<Int> {
+    return RGBA(redInt * k, greenInt * k, blueInt * k, alphaInt * k)
+}
+
+internal infix fun RGBA<Byte>.times_bf(k: Float): RGBA<Float> {
+    return RGBA(redInt * k, greenInt * k, blueInt * k, alphaInt * k)
+}
+
+internal infix fun RGBA<Byte>.times_bd(k: Double): RGBA<Double> {
+    return RGBA(redInt * k, greenInt * k, blueInt * k, alphaInt * k)
+}
+
+internal infix fun RGBA<Int>.times_ii(k: Int): RGBA<Int> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
+
+internal infix fun RGBA<Int>.times_if(k: Float): RGBA<Float> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
+
+internal infix fun RGBA<Int>.times_id(k: Double): RGBA<Double> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
+
+internal infix fun RGBA<Float>.times_ff(k: Float): RGBA<Float> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
+
+internal infix fun RGBA<Float>.times_fd(k: Double): RGBA<Double> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
+
+internal infix fun RGBA<Double>.times_dd(k: Double): RGBA<Double> {
+    return RGBA(red * k, green * k, blue * k, alpha * k)
+}
--- src/org/koherent/image/Typed.kt
+++ src/org/koherent/image/Typed.kt
@@ -12,6 +12,15 @@ sealed class ConvolutionType<I, W, O> {
     object FFF : ConvolutionType<Float, Float, Float>()
     object FDF : ConvolutionType<Float, Double, Float>()
     object DDD : ConvolutionType<Double, Double, Double>()
+    object B4II4 : ConvolutionType<RGBA<Byte>, Int, RGBA<Int>>()
+    object B4FB4 : ConvolutionType<RGBA<Byte>, Float, RGBA<Byte>>()
+    object B4DB4 : ConvolutionType<RGBA<Byte>, Double, RGBA<Byte>>()
+    object I4II4 : ConvolutionType<RGBA<Int>, Int, RGBA<Int>>()
+    object I4FI4 : ConvolutionType<RGBA<Int>, Float, RGBA<Int>>()
+    object I4DI4 : ConvolutionType<RGBA<Int>, Double, RGBA<Int>>()
+    object F4FF4 : ConvolutionType<RGBA<Float>, Float, RGBA<Float>>()
+    object F4DF4 : ConvolutionType<RGBA<Float>, Double, RGBA<Float>>()
+    object D4DD4 : ConvolutionType<RGBA<Double>, Double, RGBA<Double>>()
 }
 
 fun Image<Byte>.convoluted(kernel: Image<Int>, extrapolation: Extrapolation<Byte>, type: BII = BII): Image<Int> {
@@ -109,6 +118,105 @@ fun Image<Double>.convoluted(kernel: Image<Double>, extrapolation: Extrapolation
             { a, b -> a * b },
             0.0,
             { a, b -> a + b },
+            { it }
+    )
+}
+
+fun Image<RGBA<Byte>>.convoluted(kernel: Image<Int>, extrapolation: Extrapolation<RGBA<Byte>>, type: B4II4 = B4II4): Image<RGBA<Int>> {
+    return convoluted<RGBA<Byte>, Int, RGBA<Int>, RGBA<Int>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_bi b },
+            RGBA(0, 0, 0, 0),
+            { a, b -> a plus_ii b },
+            { it }
+    )
+}
+
+fun Image<RGBA<Byte>>.convoluted(kernel: Image<Float>, extrapolation: Extrapolation<RGBA<Byte>>, type: B4FB4 = B4FB4): Image<RGBA<Byte>> {
+    return convoluted<RGBA<Byte>, Float, RGBA<Float>, RGBA<Byte>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_bf b },
+            RGBA(0.0f, 0.0f, 0.0f, 0.0f),
+            { a, b -> a plus_ff b },
+            { RGBA(it.red.toByte(), it.green.toByte(), it.blue.toByte(), it.alpha.toByte()) }
+    )
+}
+
+fun Image<RGBA<Byte>>.convoluted(kernel: Image<Double>, extrapolation: Extrapolation<RGBA<Byte>>, type: B4DB4 = B4DB4): Image<RGBA<Byte>> {
+    return convoluted<RGBA<Byte>, Double, RGBA<Double>, RGBA<Byte>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_bd b },
+            RGBA(0.0, 0.0, 0.0, 0.0),
+            { a, b -> a plus_dd b },
+            { RGBA(it.red.toByte(), it.green.toByte(), it.blue.toByte(), it.alpha.toByte()) }
+    )
+}
+
+fun Image<RGBA<Int>>.convoluted(kernel: Image<Int>, extrapolation: Extrapolation<RGBA<Int>>, type: I4II4 = I4II4): Image<RGBA<Int>> {
+    return convoluted<RGBA<Int>, Int, RGBA<Int>, RGBA<Int>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_ii b },
+            RGBA(0, 0, 0, 0),
+            { a, b -> a plus_ii b },
+            { it }
+    )
+}
+
+fun Image<RGBA<Int>>.convoluted(kernel: Image<Float>, extrapolation: Extrapolation<RGBA<Int>>, type: I4FI4 = I4FI4): Image<RGBA<Int>> {
+    return convoluted<RGBA<Int>, Float, RGBA<Float>, RGBA<Int>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_if b },
+            RGBA(0.0f, 0.0f, 0.0f, 0.0f),
+            { a, b -> a plus_ff b },
+            { RGBA(it.red.toInt(), it.green.toInt(), it.blue.toInt(), it.alpha.toInt()) }
+    )
+}
+
+fun Image<RGBA<Int>>.convoluted(kernel: Image<Double>, extrapolation: Extrapolation<RGBA<Int>>, type: I4DI4 = I4DI4): Image<RGBA<Int>> {
+    return convoluted<RGBA<Int>, Double, RGBA<Double>, RGBA<Int>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_id b },
+            RGBA(0.0, 0.0, 0.0, 0.0),
+            { a, b -> a plus_dd b },
+            { RGBA(it.red.toInt(), it.green.toInt(), it.blue.toInt(), it.alpha.toInt()) }
+    )
+}
+
+fun Image<RGBA<Float>>.convoluted(kernel: Image<Float>, extrapolation: Extrapolation<RGBA<Float>>, type: F4FF4 = F4FF4): Image<RGBA<Float>> {
+    return convoluted<RGBA<Float>, Float, RGBA<Float>, RGBA<Float>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_ff b },
+            RGBA(0.0f, 0.0f, 0.0f, 0.0f),
+            { a, b -> a plus_ff b },
+            { it }
+    )
+}
+
+fun Image<RGBA<Float>>.convoluted(kernel: Image<Double>, extrapolation: Extrapolation<RGBA<Float>>, type: F4DF4 = F4DF4): Image<RGBA<Float>> {
+    return convoluted<RGBA<Float>, Double, RGBA<Double>, RGBA<Float>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_fd b },
+            RGBA(0.0, 0.0, 0.0, 0.0),
+            { a, b -> a plus_dd b },
+            { RGBA(it.red.toFloat(), it.green.toFloat(), it.blue.toFloat(), it.alpha.toFloat()) }
+    )
+}
+
+fun Image<RGBA<Double>>.convoluted(kernel: Image<Double>, extrapolation: Extrapolation<RGBA<Double>>, type: D4DD4 = D4DD4): Image<RGBA<Double>> {
+    return convoluted<RGBA<Double>, Double, RGBA<Double>, RGBA<Double>>(
+            kernel,
+            extrapolation,
+            { a, b -> a times_dd b },
+            RGBA(0.0, 0.0, 0.0, 0.0),
+            { a, b -> a plus_dd b },
             { it }
     )
 }
